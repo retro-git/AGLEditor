47a5f8569186b55e8da42c3bc5df766104047818
retro <43223593+retro-git@users.noreply.github.com>
2023-05-01 15:41:29 +0100

    scaffolding

---
+++ Commands/CommandBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AGLEditor.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        // WPF will automatically subscribe to this event for each property that binds to this class.
+        public event EventHandler? CanExecuteChanged;
+
+        public virtual bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object? parameter);
+
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ Stores/NavigationStore.cs
@@ -0,0 +1,32 @@
+﻿using AGLEditor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGLEditor.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel?.Dispose();
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+
+        public event Action CurrentViewModelChanged;
+    }
+}
